909766875f8a67587e8dc975e15f4bc5d862df12
Yanjin Li <yanjinli502@gmail.com>
2024-08-08 09:59:58 +0800

    修改路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,5 +8,6 @@ export default defineUserConfig({
 
   lang: "zh-CN",
   title: "你好， VuePress ！",
-  description: "这是我的第一个 VuePress 站点",
+  description: "这是我的 VuePress 站点",
+  base: "/vuepress-demo/",
 });
